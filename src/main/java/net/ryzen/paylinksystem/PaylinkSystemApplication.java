6bb0ac6a7ba73eb6c4cf79494ef0c7c4c7822eda
mrifqyz <rifqy.lxgw@gmail.com>
2025-11-12 17:07:47 +0900

    initial commit

---
+++ src/main/java/net/ryzen/paylinksystem/PaylinkSystemApplication.java
@@ -0,0 +1,13 @@
+package net.ryzen.paylinksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaylinkSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaylinkSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ryzen/paylinksystem/PaylinkSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ryzen.paylinksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaylinkSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
