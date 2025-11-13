92fa4378ae28d8ac662f80526cd738a6bf73ccbc
mrifqyz <rifqy.lxgw@gmail.com>
2025-11-13 09:50:44 +0900

    implement init jpa repository

---
+++ src/main/java/net/ryzen/paylinksystem/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package net.ryzen.paylinksystem.repository;
+
+import net.ryzen.paylinksystem.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
+}
