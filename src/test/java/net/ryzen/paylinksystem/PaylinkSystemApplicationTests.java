52cc7c29f4d164717ea7e54aad5531965ddd3d7c
mrifqyz <rifqy.lxgw@gmail.com>
2025-11-29 21:29:41 +0900

    update properties and test

--- src/test/java/net/ryzen/paylinksystem/PaylinkSystemApplicationTests.java
+++ src/test/java/net/ryzen/paylinksystem/PaylinkSystemApplicationTests.java
@@ -3,6 +3,9 @@ package net.ryzen.paylinksystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class PaylinkSystemApplicationTests {
 
