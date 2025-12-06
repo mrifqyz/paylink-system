ef15bde7e1257ae42510b4bf79cdde75390c7bdf
mrifqyz <rifqy.lxgw@gmail.com>
2025-12-07 00:42:05 +0900

    feat: implement update config payment client api

---
+++ src/main/java/net/ryzen/paylinksystem/module/dashboard/settings/dto/request/ConfigPaymentLinkRequestDTO.java
@@ -0,0 +1,22 @@
+package net.ryzen.paylinksystem.module.dashboard.settings.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+import net.ryzen.paylinksystem.base.request.ServiceRequest;
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ConfigPaymentLinkRequestDTO implements ServiceRequest {
+    @JsonIgnore
+    private String clientId;
+    private Integer transactionDueDate;
+    private String clientRedirectUrl;
+}
