8afe1e4566bc18d10bf534fc2b487c1dffc295d8
mrifqyz <rifqy.lxgw@gmail.com>
2025-12-21 03:09:28 +0900

    update pubsub config

--- src/test/java/net/ryzen/paylinksystem/PaylinkSystemApplicationTests.java
+++ src/test/java/net/ryzen/paylinksystem/PaylinkSystemApplicationTests.java
@@ -1,8 +1,10 @@
 package net.ryzen.paylinksystem;
 
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -11,6 +13,9 @@ import org.springframework.test.context.ActiveProfiles;
         com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubAutoConfiguration.class
 })
 class PaylinkSystemApplicationTests {
+
+    @MockBean
+    private PubSubTemplate pubSubTemplate;
 
     @Test
     void contextLoads() {
